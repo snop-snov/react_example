950f978602d864bd1957e17a4226cfc64448d7a0
Svetlana Saveleva <ssaweleva@gmail.com>
2014-01-11 07:46:58 +0000

    add document.ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require turbolinks
 //= require react
 //= require bootstrap
+//= require todo-list.js.jsx
--- app/assets/javascripts/todo-list.js.jsx
+++ app/assets/javascripts/todo-list.js.jsx
@@ -1,5 +1,7 @@
 /** @jsx React.DOM */
-React.renderComponent(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+$(function(){
+  React.renderComponent(
+    <h1>Hello, world!</h1>,
+    document.getElementById('example')
+  );
+});
