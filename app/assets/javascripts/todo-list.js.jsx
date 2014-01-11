957e7d017446ae0373ba086d7c464476731b2cc6
Seliverstov Maxim <maxim.web.developer@gmail.com>
2014-01-11 08:18:33 +0000

    Add taskItem component.

--- app/assets/javascripts/todo-list.js.jsx
+++ app/assets/javascripts/todo-list.js.jsx
@@ -1,15 +1,32 @@
 /** @jsx React.DOM */
 $(function(){
+  var data = [
+    '1 task',
+    '2 task',
+    '3 task'
+  ];
+
   var todoListContainer = React.createClass({
     render: function() {
+        this.props.data.map(function(text){console.log(text)});
       return (
         <div>
           <div><h1>TODO list</h1></div>
-          <div>taskList</div>
+
+          <ul>{this.props.data.map(function(text){return (<taskItem text={text} />)})}</ul>
         </div>
         )
     }
   })
+
+  var taskItem = React.createClass({
+    render: function() {
+        return (
+            <li>{this.props.text}</li>
+        );
+    }
+  });
+
   // var taskList = React.createClass({
   //   render: function() {
   //     return (
@@ -17,7 +34,7 @@ $(function(){
   //   }
   // })
   React.renderComponent(
-    <todoListContainer/>,
+    <todoListContainer data={data}/>,
     document.getElementById('todo_component')
   );
 });
