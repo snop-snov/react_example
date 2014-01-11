fe8e80f739c201c266d40c09180b72d8a534083e
Seliverstov Maxim <maxim.web.developer@gmail.com>
2014-01-11 10:11:29 +0000

    tefactory todo.js

--- app/assets/javascripts/todo-list.js.jsx
+++ app/assets/javascripts/todo-list.js.jsx
@@ -5,89 +5,49 @@ $(function(){
     getInitialState: function() {
       return {items: [], text: '', count: 0};
     },
-    render: function() {
-      return (
-        <div>
-          <todoListHeader />
-          <taskList items={this.state.items} />
-        </div>
-      );
-    }
-  });
-
-  var todoListHeader = React.createClass({
-    render: function() {
-      return (
-        <div>
-          <h1>TODO list</h1>
-          <taskForm />
-        </div>
-      );
-    }
-  });
-
-
-  var taskForm = React.createClass({
     formSubmit: function(e) {
       e.preventDefault();
       newItem = {id: this.state.count, text: this.state.text};
       newCount = this.state.count + 1;
+      newItems = this.state.items.concat(newItem)
       this.setState({
-        items: this.state.items.concat(newItem),
+        items: newItems,
         count: newCount,
         text: ''
       });
-    }
+    },
     inputChange: function(e) {
       this.setState({
         text: e.target.value
       })
-    }
+    },
+
     render: function() {
       return (
-        <div className="form-group">
-          <form className="form-inline" onSubmit={this.formSubmit}>
-            <input className="form-control" onChange={this.inputChange}/>
-            <button className="btn btn-default">Add task</button>
-          </form>
+        <div>
+          <h1>TODO list</h1>
+          <div className="form-group">
+            <form className="form-inline" onSubmit={this.formSubmit}>
+              <input className="form-control" onChange={this.inputChange} value={this.state.text}/>
+              <button className="btn btn-default">Add task</button>
+            </form>
+          </div>
+          <ul>
+            {this.state.items.map(
+              function(item){
+                return (
+                  <li>{item.text}</li>
+                );
+              }
+            )}
+          </ul>
         </div>
-      )
-    }
-  })
-
-
-  var taskList = React.createClass({
-    render: function() {
-      return (
-        <ul>
-          {this.props.items.map(
-            function(item){
-              return (
-                <taskItem text={item.text} />
-              );
-            }
-          )}
-        </ul>
       );
     }
   });
 
-  var taskItem = React.createClass({
-    render: function() {
-      return (
-          <li>{this.props.text}</li>
-      );
-    }
-  });
-
-  // var example = React.createClass({
-  //   render: function() {
-  //     return (
-  //       )
-  //   }
-  // })
   React.renderComponent(
-    <todoListContainer data={data}/>,
+    <todoListContainer />,
     document.getElementById('todo_component')
   );
 });
